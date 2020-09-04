947506630be497d0110e8e04f4d7a020845e5616
haein25 <fkdl1223@naver.com>
2020-09-04 22:42:52 +0900

    /users/signup_form.do 요청 처리 구현

--- Test01/src/main/java/com/haein25/test/users/controller/UsersController.java
+++ Test01/src/main/java/com/haein25/test/users/controller/UsersController.java
@@ -2,6 +2,7 @@ package com.haein25.test.users.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.haein25.test.users.service.UsersService;
 
@@ -9,5 +10,11 @@ import com.haein25.test.users.service.UsersService;
 public class UsersController {
 	@Autowired
 	private UsersService service;
+	
+	//회원 가입 폼 요청 처리 
+	@RequestMapping("/users/signup_form")
+	public String signupForm() {
+		return "users/signup_form";
+	}
 
 }
