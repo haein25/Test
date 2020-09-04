99f543702f57d55852e9160bb2080e14356285e2
haein25 <fkdl1223@naver.com>
2020-09-05 01:58:14 +0900

    카페글 삭제기능 구현

--- Test01/src/main/java/com/haein25/test/cafe/service/CafeServiceImpl.java
+++ Test01/src/main/java/com/haein25/test/cafe/service/CafeServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.haein25.test.cafe.dao.CafeDao;
 import com.haein25.test.cafe.dto.CafeDto;
+import com.haein25.test.exception.NotDeleteException;
 
 @Service
 public class CafeServiceImpl implements CafeService{
@@ -145,5 +146,18 @@ public class CafeServiceImpl implements CafeService{
 	@Override
 	public void updateContent(CafeDto dto) {
 		cafeDao.update(dto);
+	}
+	
+	@Override
+	public void deleteContent(int num, HttpServletRequest request) {
+		//1. 삭제할 글의 정보를 읽어온다.
+		CafeDto dto=cafeDao.getData(num);
+		//2. 본인이 작성한 글이 아닌경우 에러 처리를한다 (예외를 발생시킨다)
+		String id=(String)request.getSession().getAttribute("id");
+		//만일 로그인된 아이디와 글 작성자가 다르면
+		if(!id.equals(dto.getWriter())) {
+			throw new NotDeleteException("남의 글 지우기 없기!");
+		}		// TODO Auto-generated method stub
+		cafeDao.delete(num);
 	}
 }
