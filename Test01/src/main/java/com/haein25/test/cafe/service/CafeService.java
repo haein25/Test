cda1444933371d47aeea50b4165a88c3d6336e41
haein25 <fkdl1223@naver.com>
2020-09-05 01:53:27 +0900

    카페 새글 저장기능 구현

--- Test01/src/main/java/com/haein25/test/cafe/service/CafeService.java
+++ Test01/src/main/java/com/haein25/test/cafe/service/CafeService.java
@@ -2,7 +2,10 @@ package com.haein25.test.cafe.service;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.haein25.test.cafe.dto.CafeDto;
+
 public interface CafeService {
 	public void getList(HttpServletRequest request);
 	public void getDetail(HttpServletRequest request);
+	public void saveContent(CafeDto dto);
 }
