89377c91748a24a3532a111c59b8e0495f1ebf43
haein25 <fkdl1223@naver.com>
2020-09-04 22:52:10 +0900

    /users/checkid.do 요청처리 구현

--- Test01/src/main/java/com/haein25/test/users/controller/UsersController.java
+++ Test01/src/main/java/com/haein25/test/users/controller/UsersController.java
@@ -1,8 +1,12 @@
 package com.haein25.test.users.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.haein25.test.users.service.UsersService;
 
@@ -17,4 +21,11 @@ public class UsersController {
 		return "users/signup_form";
 	}
 
+	//아이디가 존재하는지 여부를 처리하는 요청처리
+	@RequestMapping("/users/checkid")
+	@ResponseBody
+	public Map<String, Object> checkid(@RequestParam String inputId){
+		//service  가 리턴해주는 Map 객체를 리턴한다.
+		return service.isExistId(inputId);
+	}
 }
--- Test01/src/main/java/com/haein25/test/users/dao/UsersDao.java
+++ Test01/src/main/java/com/haein25/test/users/dao/UsersDao.java
@@ -1,5 +1,5 @@
 package com.haein25.test.users.dao;
 
 public interface UsersDao {
-
+	public boolean isExist(String inputId);
 }
