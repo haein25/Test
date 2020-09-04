5a273bfd11ec011b9195311dbe195adf1728b01f
haein25 <fkdl1223@naver.com>
2020-09-04 23:02:02 +0900

    /users/login.do 요청처리 구현

--- Test01/src/main/java/com/haein25/test/users/dao/UsersDao.java
+++ Test01/src/main/java/com/haein25/test/users/dao/UsersDao.java
@@ -5,4 +5,5 @@ import com.haein25.test.users.dto.UsersDto;
 public interface UsersDao {
 	public boolean isExist(String inputId);
 	public void insert(UsersDto dto);
+	public boolean isValid(UsersDto dto);
 }
--- Test01/src/main/java/com/haein25/test/users/service/UsersService.java
+++ Test01/src/main/java/com/haein25/test/users/service/UsersService.java
@@ -2,9 +2,14 @@ package com.haein25.test.users.service;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+
 import com.haein25.test.users.dto.UsersDto;
 
 public interface UsersService {
 	public Map<String, Object> isExistId(String inputId);
 	public void addUser(UsersDto dto);
+	public void loginProcess(UsersDto dto, ModelAndView mView, HttpSession session);
 }
